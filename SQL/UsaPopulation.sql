20e39d0aa5f2c9adc4e05a5f16b1ae84bf4db9fc
Mustafa Selim Özen <44001855+selimozen@users.noreply.github.com>
2021-07-23 13:25:30 +0300

    Update UsaPopulation.sql

--- SQL/UsaPopulation.sql
+++ SQL/UsaPopulation.sql
@@ -1,3 +1,5 @@
+--Eng: Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
+-- Tr: 100.000'in üzerinde nüfusu olan ABD şehilerini seçiniz.
 SELECT * FROM CITY WHERE 
     COUNTRYCODE = 'USA' 
     AND POPULATION > 100000;
